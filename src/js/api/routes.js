b6b8b1441dc66e58a4ed11066aa64c0746462272
adhamali450 <adhamali_4500@outlook.com>
2023-05-15 14:46:11 +0200

    :rewind: Reverting from using proxy in vite.config.js

--- src/js/api/routes.js
+++ src/js/api/routes.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 // TODO: Remove when fixing the vite.config.js proxy issue
-// const API_URL = "https://genius-unofficial-api.vercel.app/api";
+const API_URL = "https://genius-unofficial-api.vercel.app/api";
 
 export const getCORSImage = async (url) => {
-  const res = await axios.get("/api/cors?url=" + encodeURIComponent(url));
+  const res = await axios.get(API_URL + "/cors?url=" + encodeURIComponent(url));
 
   return res;
 };
 
 export const getLyrics = async (id) => {
-  const res = await axios.get(`/api/song/lyrics/${id}`);
+  const res = await axios.get(API_URL + `/song/lyrics/${id}`);
 
   return res;
 };
 
 export const getColors = async (url) => {
-  const res = await axios.get("/api/song/colors", {
+  const res = await axios.get(API_URL + "/song/colors", {
     params: { url: url },
   });
 
@@ -24,7 +24,7 @@ export const getColors = async (url) => {
 };
 
 export const search = async (query) => {
-  const res = await axios.get(`/api/search`, {
+  const res = await axios.get(API_URL + `/search`, {
     params: {
       query: query,
     },
