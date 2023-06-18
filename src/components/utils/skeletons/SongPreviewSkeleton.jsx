65337019db276329631a8d2d43e881f849fe0fc9
adhamali450 <adhamali_4500@outlook.com>
2023-06-18 23:49:29 +0200

    :adhesive_bandage: Fix SongPreview skeleton

--- src/components/utils/skeletons/SongPreviewSkeleton.jsx
+++ src/components/utils/skeletons/SongPreviewSkeleton.jsx
@@ -1,17 +1,20 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-const SongPreviewSkeleton = ({ className }) => {
+const SongPreviewSkeleton = ({ className, height }) => {
   return (
     <div className={`${className} max-h-full overflow-hidden flex gap-3`}>
       <SkeletonTheme baseColor="#dedede" highlightColor="#c8c8c8">
-        <Skeleton
-          height="100%"
-          width="auto"
+        <div
           style={{
+            height: "100%",
+            width: "auto",
             aspectRatio: "1/1",
           }}
-        />
+        >
+          <Skeleton width="100%" height="100%" />
+        </div>
+
         <div className="flex flex-col justify-center items-start">
           <Skeleton width={128} height={8} style={{ marginBottom: "-10px" }} />
           <Skeleton width={96} height={8} />
