96a136d9506a775a089505c335eaeadd28fdcf80
adhamali450 <adhamali_4500@outlook.com>
2023-08-22 17:57:34 +0200

    :bug: Showing footer text by default

--- src/components/lyrics-card/LyricsCard.jsx
+++ src/components/lyrics-card/LyricsCard.jsx
@@ -42,7 +42,7 @@ const LyricsCard = forwardRef(
     { cardInfo, lyricsData, aspectRatio = "1:1", onDownload = () => {} },
     ref
   ) => {
-    let { title = "", artist = "" } = cardInfo;
+    let { title = "Song Title", artist = "Artist Name" } = cardInfo;
 
     const [isFileDragged, setIsFileDragged] = useState(false);
     const [showDragOverlay, setShowDragOverlay] = useState(true);
@@ -286,15 +286,13 @@ const LyricsCard = forwardRef(
 
             {/* Aux footer */}
             <footer className={`${styles["aux-footer"]}`}>
-              {artist && title && (
-                <EditableLabel
-                  text={footerText}
-                  childrenStyle={{
-                    color: "white",
-                  }}
-                  onChange={setFooterText}
-                />
-              )}
+              <EditableLabel
+                text={footerText}
+                childrenStyle={{
+                  color: "white",
+                }}
+                onChange={setFooterText}
+              />
             </footer>
           </div>
         </main>
@@ -306,13 +304,11 @@ const LyricsCard = forwardRef(
           }}
         >
           <div className={styles["info"]}>
-            {artist && title && (
-              <EditableLabel
-                className={`${styles["editable-label"]}`}
-                text={footerText}
-                onChange={setFooterText}
-              />
-            )}
+            <EditableLabel
+              className={`${styles["editable-label"]}`}
+              text={footerText}
+              onChange={setFooterText}
+            />
           </div>
 
           <button className={styles["logo"]} onClick={toggleLogoSize}>
